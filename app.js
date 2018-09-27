66502deb876effa1caea10d0a566812273500937
jarriagada <jarriagada@bancointernacional.cl>
2018-09-27 16:35:55 -0300

    Sección 11 terminada - CRUD Imagenes

--- app.js
+++ app.js
@@ -9,14 +9,19 @@ var app = express();
 
 //Body Parser
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //Importar rutas
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 var loginRoutes = require('./routes/login');
 
 //Conexion a la base de datos
@@ -28,8 +33,18 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 });
 
+//Server index config
+//var serveIndex = require('serve-index');
+//app.use(express.static(__dirname + '/'))
+//app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
 //Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 app.use('/login', loginRoutes);
 app.use('/', appRoutes);
 
